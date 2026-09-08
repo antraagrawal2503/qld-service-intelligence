f2fa7e7d3157bdd4ab2711f2d4fd42fdebc9ab88
antraagrawal2503 <antraagrawal2503@gmail.com>
2026-09-08 23:15:58 +1000

    Configure PostgreSQL database connectivity

--- src/test/java/com/antra/qldserviceintelligence/QldServiceIntelligenceApplicationTests.java
+++ src/test/java/com/antra/qldserviceintelligence/QldServiceIntelligenceApplicationTests.java
@@ -1,13 +1,34 @@
 package com.antra.qldserviceintelligence;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QldServiceIntelligenceApplicationTests {
+
+	@Autowired
+	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void connectsToIsolatedTestDatabase() throws Exception {
+		try (var connection = dataSource.getConnection();
+				var statement = connection.createStatement();
+				var result = statement.executeQuery("SELECT 1")) {
+			assertThat(connection.getMetaData().getURL()).startsWith("jdbc:h2:mem:");
+			assertThat(result.next()).isTrue();
+			assertThat(result.getInt(1)).isEqualTo(1);
+		}
 	}
 
 }
